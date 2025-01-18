4131006560500cc06eda18a1676fafbb1d10c03e
noob762 <noobopskdh@gmail.com>
2025-01-19 00:34:20 +0530

    Update LSR_VIP_DDOS.py

--- LSR_VIP_DDOS.py
+++ LSR_VIP_DDOS.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot tokSasen here
-bot = telebot.TeleBot('7238502739:AAHUs-v5Cswjoi3wcfB_OjLTg')
+bot = telebot.TeleBot('7244168157:AAEqCRFu7ypIS2zrk6iFU-bxhkodFPZIoZ8')
 
 # Admin user IDs
-admin_id = ["5599402910"]
+admin_id = ["1245544470"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
